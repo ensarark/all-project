6eca8f1b14c9192b08167dde1ae31cb37f0fde09
Ensar Korkmaz <120710216+ensarark@users.noreply.github.com>
2023-06-04 15:14:49 +0300

    all-projects
    Tüm projeler

---
+++ Projeler.js
@@ -0,0 +1,53 @@
+function btn(){
+    document.getElementById("gizle").style.display="inline";
+    setTimeout(()=> {
+        window.location.href="Projeler/odev-1-main/ödev-11.html"
+     }
+     ,2000);
+    
+}
+function btn2(){
+    document.getElementById("gizle").style.display="inline";
+    setTimeout(()=> {
+        window.location.href="Projeler/ödev-12/ödev-12.html"
+     }
+     ,2000);
+    
+}
+function btn3(){
+    document.getElementById("gizle").style.display="inline";
+    setTimeout(()=> {
+        window.location.href="Projeler/wep template/template.html"
+     }
+     ,2000);
+    
+}
+
+function btn4(){
+    document.getElementById("gizle").style.display="inline";
+    setTimeout(()=> {
+        window.location.href="Projeler/web-5/Giris.html"
+     }
+     ,2000);
+    
+}
+
+function btn5(){
+    document.getElementById("gizle").style.display="inline";
+    setTimeout(()=> {
+        window.location.href="https://github.com/ensarark/web-3-mvc"
+     }
+     ,2000);
+    
+}
+
+function btn6(){
+    document.getElementById("gizle").style.display="inline";
+    setTimeout(()=> {
+        window.location.href="https://github.com/ensarark/web-4-mvc"
+     }
+     ,2000);
+    
+}
+
+
